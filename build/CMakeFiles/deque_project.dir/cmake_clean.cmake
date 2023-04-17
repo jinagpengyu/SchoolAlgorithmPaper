f4be3e0487a4a06c820676125b2e2fe829004f43
jiangpengyu <1770693880@qq.com>
2023-04-17 14:36:41 +0800

    first

---
+++ build/CMakeFiles/deque_project.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/deque_project.dir/deque.cpp.o"
+  "CMakeFiles/deque_project.dir/deque.cpp.o.d"
+  "deque_project"
+  "deque_project.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/deque_project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/deque_project.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for deque_project.
+# This may be replaced when dependencies are built.
