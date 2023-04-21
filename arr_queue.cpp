4cf40f5213baff827154947b4bd7c896375696f8
jiangpengyu <1770693880@qq.com>
2023-04-21 13:34:20 +0800

    change

---
+++ arr_queue.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<cstdio>
+#include<cstring>
+#include<cstdlib>
+using namespace std;
+#define maxn 10001000
+int q[maxn], a[maxn];
+int n, k;
+int main(){
+
+    return 0;
+}
--- deque.h
+++ deque.h
@@ -1,25 +1,29 @@
+#include <cstdarg>
+#include <cstdio>
 #include<iostream>
-#include<cstdlib>
-#include<deque>
+#include<queue>
+#include <utility>
+using namespace std;
 
-void single_deque(){    
-    std::deque<int> deque = {};
-    int n, m;
-    std::cin >> n >> m;
-    int arr[n];
-    for(int i = 0;i < n; i ++){
-        std::cin >> arr[i];
-    }
-    int arr2[n];
-    for(int i = 0 ; i < n ; i++){
-        if(i == 0){
-            arr2[0] = 0;
-            deque.push_back(arr[0]);
+const int max=2e6+6;
+void single_deque(){
+    int n, m;    
+    scanf("%d %d", &n, &m);
+    deque<pair<int, int>> dq ;
+    for(int i = 0;i < n;i++){
+        int num = 0;
+        scanf("%d",&num);
+        if(dq.size() && dq.front().first < i-m){
+            dq.pop_front();
+        }
+        if(dq.size()){
+            printf("%d\n" ,dq.front().second);
         }else{
-            if()
+            puts("0");
         }
-        if(deque.size() < m){
-            deque.push_back(arr[i]);
+        while (dq.size() && dq.back().second >= num) {
+            dq.pop_back();
         }
+        dq.push_back({i,num});
     }
 }
